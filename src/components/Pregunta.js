6bfec7828af89228763187ec815c854cec420415
misanc21 <misanc21@gmail.com>
2020-08-24 14:20:41 -0500

    mostrar error de validacion

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,19 +1,40 @@
-import React, { Fragment } from 'react'
-
+import React, { Fragment, useState } from 'react'
+import Error from './Error'
 const Pregunta = () => {
+
+    const [cantidad, setCantidad] = useState(0);
+    const [error, setError] = useState(false)
+
+    const definirPresupuesto = e => {
+        setCantidad(parseInt(e.target.value, 10))
+    }
+
+    const agregarPresupuesto = e => {
+        e.preventDefault()
+
+        if (cantidad < 1 || isNaN(cantidad)) {
+            setError(true)
+            return;
+        }
+        setError(false)
+
+
+    }
+
     return (
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
-
-            <form>
+            {error ? <Error mensaje="el presupuesto debe de ser mayor a cero" /> : null}
+            <form onSubmit={agregarPresupuesto}>
                 <input
-                    type = "number"
-                    className = "u-full-width"
-                    placeholder = "Coloca tu presupuesto"
+                    type="number"
+                    className="u-full-width"
+                    placeholder="Coloca tu presupuesto"
+                    onChange={definirPresupuesto}
                 />
                 <input
-                    type = "submit"
-                    className = "button-primary u-full-width"
+                    type="submit"
+                    className="button-primary u-full-width"
                 />
             </form>
         </Fragment>
